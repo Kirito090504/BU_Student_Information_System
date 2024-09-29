af89507402735591d32ba7f3faf57a833986abe6
nezueris <dreymamuri@gmail.com>
2024-09-29 21:12:22 +0800

    SheetPage1 added (unfinished)

---
+++ StudentInformationSheet/StudentInformationSheet/StudentSheetPage1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace StudentInformationSheet
+{
+    partial class StudentSheetPage1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button minimizeBut;
+            System.Windows.Forms.Button exitBut;
+            System.Windows.Forms.Button uploadBut;
+            System.Windows.Forms.Button button1;
+            this.photoHolder = new System.Windows.Forms.PictureBox();
+            this.usernameTb = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            minimizeBut = new System.Windows.Forms.Button();
+            exitBut = new System.Windows.Forms.Button();
+            uploadBut = new System.Windows.Forms.Button();
+            button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.photoHolder)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // minimizeBut
+            // 
+            minimizeBut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            minimizeBut.BackColor = System.Drawing.Color.Transparent;
+            minimizeBut.BackgroundImage = global::StudentInformationSheet.Properties.Resources.No_Entry_Transparent_Background;
+            minimizeBut.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            minimizeBut.Cursor = System.Windows.Forms.Cursors.Hand;
+            minimizeBut.FlatAppearance.BorderSize = 0;
+            minimizeBut.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightBlue;
+            minimizeBut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            minimizeBut.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            minimizeBut.ForeColor = System.Drawing.Color.Indigo;
+            minimizeBut.Location = new System.Drawing.Point(1324, 19);
+            minimizeBut.Margin = new System.Windows.Forms.Padding(0);
+            minimizeBut.Name = "minimizeBut";
+            minimizeBut.Size = new System.Drawing.Size(32, 19);
+            minimizeBut.TabIndex = 9;
+            minimizeBut.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            minimizeBut.UseVisualStyleBackColor = false;
+            // 
+            // exitBut
+            // 
+            exitBut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            exitBut.BackColor = System.Drawing.Color.Transparent;
+            exitBut.BackgroundImage = global::StudentInformationSheet.Properties.Resources.Xicon_removebg_preview;
+            exitBut.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            exitBut.Cursor = System.Windows.Forms.Cursors.Hand;
+            exitBut.FlatAppearance.BorderSize = 0;
+            exitBut.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightBlue;
+            exitBut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            exitBut.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            exitBut.ForeColor = System.Drawing.Color.Indigo;
+            exitBut.Location = new System.Drawing.Point(1411, 19);
+            exitBut.Margin = new System.Windows.Forms.Padding(0);
+            exitBut.Name = "exitBut";
+            exitBut.Size = new System.Drawing.Size(32, 19);
+            exitBut.TabIndex = 10;
+            exitBut.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            exitBut.UseVisualStyleBackColor = false;
+            // 
+            // uploadBut
+            // 
+            uploadBut.BackColor = System.Drawing.Color.Transparent;
+            uploadBut.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            uploadBut.Cursor = System.Windows.Forms.Cursors.Hand;
+            uploadBut.FlatAppearance.BorderSize = 0;
+            uploadBut.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightBlue;
+            uploadBut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            uploadBut.Font = new System.Drawing.Font("Mongolian Baiti", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            uploadBut.ForeColor = System.Drawing.Color.Indigo;
+            uploadBut.Location = new System.Drawing.Point(1249, 360);
+            uploadBut.Margin = new System.Windows.Forms.Padding(0);
+            uploadBut.Name = "uploadBut";
+            uploadBut.Size = new System.Drawing.Size(90, 30);
+            uploadBut.TabIndex = 12;
+            uploadBut.Text = "Upload";
+            uploadBut.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            uploadBut.UseVisualStyleBackColor = false;
+            uploadBut.Click += new System.EventHandler(this.uploadBut_Click);
+            // 
+            // button1
+            // 
+            button1.BackColor = System.Drawing.Color.Transparent;
+            button1.BackgroundImage = global::StudentInformationSheet.Properties.Resources.NextBut_2_;
+            button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightBlue;
+            button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button1.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            button1.ForeColor = System.Drawing.Color.Indigo;
+            button1.Location = new System.Drawing.Point(1238, 760);
+            button1.Margin = new System.Windows.Forms.Padding(0);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(136, 31);
+            button1.TabIndex = 13;
+            button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // photoHolder
+            // 
+            this.photoHolder.BackColor = System.Drawing.Color.White;
+            this.photoHolder.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.photoHolder.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.photoHolder.Location = new System.Drawing.Point(1219, 201);
+            this.photoHolder.Name = "photoHolder";
+            this.photoHolder.Size = new System.Drawing.Size(147, 117);
+            this.photoHolder.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.photoHolder.TabIndex = 11;
+            this.photoHolder.TabStop = false;
+            // 
+            // usernameTb
+            // 
+            this.usernameTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.usernameTb.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTb.ForeColor = System.Drawing.Color.Black;
+            this.usernameTb.Location = new System.Drawing.Point(177, 185);
+            this.usernameTb.Name = "usernameTb";
+            this.usernameTb.Size = new System.Drawing.Size(415, 36);
+            this.usernameTb.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(177, 329);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(415, 36);
+            this.textBox1.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(177, 466);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(415, 36);
+            this.textBox2.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.Black;
+            this.textBox3.Location = new System.Drawing.Point(177, 606);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(415, 36);
+            this.textBox3.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ForeColor = System.Drawing.Color.Black;
+            this.textBox4.Location = new System.Drawing.Point(177, 744);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(415, 36);
+            this.textBox4.TabIndex = 18;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.ForeColor = System.Drawing.Color.Black;
+            this.textBox5.Location = new System.Drawing.Point(729, 744);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(415, 36);
+            this.textBox5.TabIndex = 23;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.ForeColor = System.Drawing.Color.Black;
+            this.textBox6.Location = new System.Drawing.Point(729, 606);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(415, 36);
+            this.textBox6.TabIndex = 22;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox7.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.ForeColor = System.Drawing.Color.Black;
+            this.textBox7.Location = new System.Drawing.Point(729, 466);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(415, 36);
+            this.textBox7.TabIndex = 21;
+            // 
+            // textBox8
+            // 
+            this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox8.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.ForeColor = System.Drawing.Color.Black;
+            this.textBox8.Location = new System.Drawing.Point(729, 329);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(415, 36);
+            this.textBox8.TabIndex = 20;
+            // 
+            // textBox9
+            // 
+            this.textBox9.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox9.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox9.ForeColor = System.Drawing.Color.Black;
+            this.textBox9.Location = new System.Drawing.Point(729, 185);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(415, 36);
+            this.textBox9.TabIndex = 19;
+            // 
+            // StudentSheetPage1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::StudentInformationSheet.Properties.Resources.StudentSheetPage1_1_;
+            this.ClientSize = new System.Drawing.Size(1500, 900);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.usernameTb);
+            this.Controls.Add(button1);
+            this.Controls.Add(uploadBut);
+            this.Controls.Add(this.photoHolder);
+            this.Controls.Add(exitBut);
+            this.Controls.Add(minimizeBut);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "StudentSheetPage1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StudentSheetPage1";
+            ((System.ComponentModel.ISupportInitialize)(this.photoHolder)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox photoHolder;
+        private System.Windows.Forms.TextBox usernameTb;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+    }
+}
