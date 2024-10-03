5c207118f0691bc54f388c6d757b3d38ed38dd4b
chris1320 <chris1320is@protonmail.com>
2024-10-03 21:48:13 +0800

    feat: update db schema
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- Server/db_schema.sql
+++ Server/db_schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Oct 02, 2024 at 05:09 PM
+-- Generation Time: Oct 03, 2024 at 01:45 PM
 -- Server version: 9.0.1
 -- PHP Version: 8.2.24
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -36,15 +36,6 @@ CREATE TABLE `academic_history` (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'Information about the students academic history.';
 -- --------------------------------------------------------
 --
--- Table structure for table `cities`
---
-
-CREATE TABLE `cities` (
-  `id` int NOT NULL,
-  `description` varchar(32) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'A list of valid cities.';
--- --------------------------------------------------------
---
 -- Table structure for table `contact_information`
 --
 
@@ -53,31 +44,6 @@ CREATE TABLE `contact_information` (
   `contact_number` varchar(16) DEFAULT NULL COMMENT 'The students contact number with country code.',
   `email_address` text COMMENT 'The students email address.'
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'Students contact information';
--- --------------------------------------------------------
---
--- Table structure for table `genders`
---
-
-CREATE TABLE `genders` (
-  `id` int NOT NULL,
-  `description` varchar(32) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
---
--- Dumping data for table `genders`
---
-
-INSERT INTO `genders` (`id`, `description`)
-VALUES (2, 'Female'),
-  (1, 'Male');
--- --------------------------------------------------------
---
--- Table structure for table `nationalities`
---
-
-CREATE TABLE `nationalities` (
-  `id` int NOT NULL,
-  `description` varchar(32) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'A table containing valid values for the students nationality.';
 -- --------------------------------------------------------
 --
 -- Table structure for table `permanent_addresses`
@@ -110,25 +76,6 @@ CREATE TABLE `present_addresses` (
   `city` int NOT NULL COMMENT 'The city of the students present address.',
   `province` int NOT NULL COMMENT 'The province of the students present address.'
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'The present addresses of the students.';
--- --------------------------------------------------------
---
--- Table structure for table `provinces`
---
-
-CREATE TABLE `provinces` (
-  `id` int NOT NULL,
-  `description` varchar(32) NOT NULL,
-  `zip_code` int NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'A list of valid provinces.';
--- --------------------------------------------------------
---
--- Table structure for table `religions`
---
-
-CREATE TABLE `religions` (
-  `id` int NOT NULL,
-  `description` varchar(64) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'A table containing a list of religions.';
 -- --------------------------------------------------------
 --
 -- Table structure for table `students`
@@ -210,34 +157,15 @@ VALUES (
 ALTER TABLE `academic_history`
 ADD PRIMARY KEY (`id`);
 --
--- Indexes for table `cities`
---
-ALTER TABLE `cities`
-ADD PRIMARY KEY (`id`);
---
 -- Indexes for table `contact_information`
 --
 ALTER TABLE `contact_information`
 ADD PRIMARY KEY (`id`);
 --
--- Indexes for table `genders`
---
-ALTER TABLE `genders`
-ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `description` (`description`);
---
--- Indexes for table `nationalities`
---
-ALTER TABLE `nationalities`
-ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `description` (`description`);
---
 -- Indexes for table `permanent_addresses`
 --
 ALTER TABLE `permanent_addresses`
-ADD PRIMARY KEY (`id`),
-  ADD KEY `permanent_addresses_city_cities_id` (`city`),
-  ADD KEY `permanent_addresses_province_provinces_id` (`province`);
+ADD PRIMARY KEY (`id`);
 --
 -- Indexes for table `personalities`
 --
@@ -247,31 +175,12 @@ ADD PRIMARY KEY (`id`);
 -- Indexes for table `present_addresses`
 --
 ALTER TABLE `present_addresses`
-ADD PRIMARY KEY (`id`, `city`, `province`),
-  ADD KEY `present_addresses_city_cities_id` (`city`),
-  ADD KEY `present_addresses_province_provinces_id` (`province`);
---
--- Indexes for table `provinces`
---
-ALTER TABLE `provinces`
-ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `description` (`description`),
-  ADD UNIQUE KEY `zip_code` (`zip_code`);
---
--- Indexes for table `religions`
---
-ALTER TABLE `religions`
-ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `description` (`description`);
+ADD PRIMARY KEY (`id`) USING BTREE;
 --
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
-ADD PRIMARY KEY (`student_number`),
-  ADD KEY `students_gender_genders_id` (`gender`),
-  ADD KEY `students_nationality_nationalities_id` (`nationality`),
-  ADD KEY `students_citizenship_nationalities_id` (`citizenship`),
-  ADD KEY `students_religion_religions_id` (`religion`);
+ADD PRIMARY KEY (`student_number`);
 --
 -- Indexes for table `student_family`
 --
@@ -288,27 +197,11 @@ ADD PRIMARY KEY (`user_id`),
 --
 
 --
--- AUTO_INCREMENT for table `genders`
---
-ALTER TABLE `genders`
-MODIFY `id` int NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 3;
---
--- AUTO_INCREMENT for table `nationalities`
---
-ALTER TABLE `nationalities`
-MODIFY `id` int NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `religions`
---
-ALTER TABLE `religions`
-MODIFY `id` int NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
 MODIFY `user_id` int NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 2;
+  AUTO_INCREMENT = 5;
 --
 -- Constraints for dumped tables
 --
@@ -327,8 +220,7 @@ ADD CONSTRAINT `contact_information_id_students_student_number` FOREIGN KEY (`id
 -- Constraints for table `permanent_addresses`
 --
 ALTER TABLE `permanent_addresses`
-ADD CONSTRAINT `permanent_addresses_city_cities_id` FOREIGN KEY (`city`) REFERENCES `cities` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `permanent_addresses_id_students_student_number` FOREIGN KEY (`id`) REFERENCES `students` (`student_number`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `permanent_addresses_id_students_student_number` FOREIGN KEY (`id`) REFERENCES `students` (`student_number`) ON DELETE CASCADE ON UPDATE CASCADE;
 --
 -- Constraints for table `personalities`
 --
@@ -338,17 +230,7 @@ ADD CONSTRAINT `personalities_id_students_student_number` FOREIGN KEY (`id`) REF
 -- Constraints for table `present_addresses`
 --
 ALTER TABLE `present_addresses`
-ADD CONSTRAINT `present_addresses_city_cities_id` FOREIGN KEY (`city`) REFERENCES `cities` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `present_addresses_id_students_student_number` FOREIGN KEY (`id`) REFERENCES `students` (`student_number`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `present_addresses_province_provinces_id` FOREIGN KEY (`province`) REFERENCES `provinces` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
---
--- Constraints for table `students`
---
-ALTER TABLE `students`
-ADD CONSTRAINT `students_citizenship_nationalities_id` FOREIGN KEY (`citizenship`) REFERENCES `nationalities` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `students_gender_genders_id` FOREIGN KEY (`gender`) REFERENCES `genders` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `students_nationality_nationalities_id` FOREIGN KEY (`nationality`) REFERENCES `nationalities` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `students_religion_religions_id` FOREIGN KEY (`religion`) REFERENCES `religions` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ADD CONSTRAINT `present_addresses_id_students_student_number` FOREIGN KEY (`id`) REFERENCES `students` (`student_number`) ON DELETE CASCADE ON UPDATE CASCADE;
 --
 -- Constraints for table `student_family`
 --
